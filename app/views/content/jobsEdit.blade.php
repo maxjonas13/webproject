642b871a3e8051c2b21625c87604f161715c7bd4
maxjonas13 <jonas.vandersande@student.kdg.be>
2014-10-29 14:27:26 +0100

    change vieuw

--- app/views/content/jobsEdit.blade.php
+++ app/views/content/jobsEdit.blade.php
@@ -1,85 +1,106 @@
-<?php
-$it = FALSE;
-$languages = FALSE;
-$finances = FALSE;
-$repairs = FALSE;
-$math = FALSE;
-$art = FALSE;
-$cooking = FALSE;
-$programming = FALSE;
-?>
-@foreach($data->category as $item)
-	<?php echo $item->categoryName;?>
-	@if($item->categoryName == 'IT')
-		<?php $it = TRUE; ?>
-	@endif
-	@if($item->categoryName == 'Languages')
-		<?php $languages = TRUE; ?>
-	@endif
-	@if($item->categoryName == 'Finances')
-		<?php $finances = TRUE; ?>
-	@endif
-	@if($item->categoryName == 'Repair')
-		<?php $repairs = TRUE; ?>
-	@endif
-	@if($item->categoryName == 'Math')
-		<?php $math = TRUE; ?>
-	@endif
-	@if($item->categoryName == 'Art')
-		<?php $art = TRUE; ?>
-	@endif
-	@if($item->categoryName == 'Cooking')
-		<?php $cooking = TRUE; ?>
-	@endif
-	@if($item->categoryName == 'Programming')
-		<?php $programming = TRUE; ?>
-	@endif
-@endforeach
+@extends("layout.defaultdetails")
 
-@if($errors->count() > 0) 
-	@foreach($errors->all() as $error)
-	<ul>
-		<li>{{$error}}</li>
-	</ul>
+@section('content')
+		<div class="row"> 
+			<div class="column col-md-2 col-sm-3"> </div>
+			<div class="column col-md-8 col-sm-6" id="profile">  
+				<h1>Edit Job</h1>
+
+	<?php
+	$it = FALSE;
+	$languages = FALSE;
+	$finances = FALSE;
+	$repairs = FALSE;
+	$math = FALSE;
+	$art = FALSE;
+	$cooking = FALSE;
+	$programming = FALSE;
+	?>
+	@foreach($data->category as $item)
+		@if($item->categoryName == 'IT')
+			<?php $it = TRUE; ?>
+		@endif
+		@if($item->categoryName == 'Languages')
+			<?php $languages = TRUE; ?>
+		@endif
+		@if($item->categoryName == 'Finances')
+			<?php $finances = TRUE; ?>
+		@endif
+		@if($item->categoryName == 'Repair')
+			<?php $repairs = TRUE; ?>
+		@endif
+		@if($item->categoryName == 'Math')
+			<?php $math = TRUE; ?>
+		@endif
+		@if($item->categoryName == 'Art')
+			<?php $art = TRUE; ?>
+		@endif
+		@if($item->categoryName == 'Cooking')
+			<?php $cooking = TRUE; ?>
+		@endif
+		@if($item->categoryName == 'Programming')
+			<?php $programming = TRUE; ?>
+		@endif
 	@endforeach
-@endif
-<!-- open form tag -->
-		{{ Form::open( array('url' => '/jobs/update') ) }}
 
-			{{ Form::text('title' , $data->title , array('placeholder'=> "Title")) }}
-			<br>
-			{{ Form::text('location' , $data->location , array('placeholder'=> "Location")) }}
-			<br>						
-			{{ Form::textarea('description' , $data->description , array('placeholder'=> "Description")) }}
-			<br>
-	
-				{{ Form::label('grouped["it"]', 'IT')}}
-				{{ Form::checkbox('grouped[it]', '', $it)}}
+	@if($errors->count() > 0) 
+		@foreach($errors->all() as $error)
+		<ul>
+			<li>{{$error}}</li>
+		</ul>
+		@endforeach
+	@endif
+	<!-- open form tag -->
+			{{ Form::open( array('url' => '/jobs/update') ) }}
 
-				{{ Form::label('languages', 'Languages')}}
-				{{ Form::checkbox('grouped[languages]', '', $languages)}}
-				
-				{{ Form::label('finances')}}
-				{{ Form::checkbox('grouped[finances]', '', $finances)}}
-				
-				{{ Form::label('repairs')}}
-				{{ Form::checkbox('grouped[repair]', '', $repairs)}}
-				
-				{{ Form::label('math', 'Math & Fysics')}}
-				{{ Form::checkbox('grouped[math]', '', $math)}}
-				
-				{{ Form::label('art', 'Art')}}
-				{{ Form::checkbox('grouped[art]', '', $art)}}
-
-				{{ Form::label('cooking', 'Cooking')}}
-				{{ Form::checkbox('grouped[cooking]', '', $cooking)}}
-
-				{{ Form::label('programming', 'Programming')}}
-				{{ Form::checkbox('grouped[programming]', '', $programming)}}
+				{{ Form::text('title' , $data->title , array('placeholder'=> "Title")) }}
+				<br>
+				{{ Form::text('location' , $data->location , array('placeholder'=> "Location")) }}
+				<br>						
+				{{ Form::textarea('description' , $data->description , array('placeholder'=> "Description")) }}
+				<br>
 		
-			<br>
-			{{ Form::submit('Voeg job toe') }}
-											
-		<!-- close form tag -->
-		{{ Form::close() }}
-		{{$data}}
+					
+					{{ Form::checkbox('grouped[it]', '', $it)}}
+					{{ Form::label('grouped["it"]', 'IT')}}
+
+					
+					{{ Form::checkbox('grouped[languages]', '', $languages)}}
+					{{ Form::label('languages', 'Languages')}}
+					
+					
+					{{ Form::checkbox('grouped[finances]', '', $finances)}}
+					{{ Form::label('finances')}}
+					
+					
+					{{ Form::checkbox('grouped[repair]', '', $repairs)}}
+					{{ Form::label('repairs')}}
+					
+					
+					{{ Form::checkbox('grouped[math]', '', $math)}}
+					{{ Form::label('math', 'Math & Fysics')}}
+					
+					
+					{{ Form::checkbox('grouped[art]', '', $art)}}
+					{{ Form::label('art', 'Art')}}
+
+					
+					{{ Form::checkbox('grouped[cooking]', '', $cooking)}}
+					{{ Form::label('cooking', 'Cooking')}}
+
+					
+					{{ Form::checkbox('grouped[programming]', '', $programming)}}
+					{{ Form::label('programming', 'Programming')}}
+			
+				<br>
+				{{ Form::submit('Voeg job toe') }}
+												
+			<!-- close form tag -->
+			{{ Form::close() }}
+
+					</div>
+		<div class="column col-md-2 col-sm-3"> </div>
+	</div>
+</div>
+
+@stop
