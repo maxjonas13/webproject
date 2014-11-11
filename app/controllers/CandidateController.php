0456cfd3961846e0e825c705e0e2cb4f124e6096
jorenvh <jorenvh@gmail.com>
2014-11-11 14:03:33 +0100

    Bug fix candidate

--- app/controllers/CandidateController.php
+++ app/controllers/CandidateController.php
@@ -3,12 +3,17 @@
 class CandidateController extends BaseController {
 
 	public function apply($id) {
-		$candidate = new Candidate;
-		$candidate->FK_jobId = 54;
-		$candidate->FK_userId = Auth::user()->PK_userId;
-		$candidate->canceled = FALSE;
+		// $candidate = new Candidate;
+		// $candidate->FK_jobId = 54;
+		// $candidate->FK_userId = Auth::user()->PK_userId;
+		// $candidate->canceled = FALSE;
 
-		$candidate->save();
+		// $candidate->save();
+		$candidate = new Candidate;
+		$check = $candidate->checkIfUserHasSolicitated($id)
+		if(!$check) {
+			$candidate->store($id);
+		}
 	}
 	
 
