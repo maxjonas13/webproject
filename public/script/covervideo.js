c737606cbb743cc53139905d0bc0298bf625b774
maxjonas13 <jonas.vandersande@student.kdg.be>
2014-11-11 17:53:20 +0100

    video-fix

--- public/script/covervideo.js
+++ public/script/covervideo.js
@@ -1,11 +1,13 @@
 $( document ).ready(function(){
+	$('#video-container').css("height", $('video').height());
 	$('#videocontent').css("height", $('#video-container').height());
 	$('#searchbar').css("margin-top", $('#videocontent').height());
 	$('.firstrow').css("margin-top", $('#videocontent').height() + 100-20);
 
 });
 $( window ).resize(function(){
-	$('#videocontent').css("height", $('video').height());
+	$('#video-container').css("height", $('video').height());
+	$('#videocontent').css("height", $('#video-container').height());
 	$('#searchbar').css("margin-top", $('#videocontent').height());
-	$('.firstrow').css("margin-top", $('#videocontent').height() + 100);
+	$('.firstrow').css("margin-top", $('#videocontent').height() + 100-20);
 });
