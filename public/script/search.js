6f404391a705c52edf536642a6b440c4beb6751e
jorenvh <jorenvh@gmail.com>
2014-11-11 12:22:22 +0100

    Search optimalisation
    - time out kleiner gemaakt
    - indien veld terug leeg wordt gemaakt een slide up slow ipv een hide
    - else functie weg gehaald

--- public/script/search.js
+++ public/script/search.js
@@ -18,50 +18,50 @@ function setATimeout(searchstring) {
 			// console.log(data);
 			showInteligence(data);
 		}).fail(function() { 
-       		$('#jobsresult').hide();
-			$('#userresult').hide();
+       		$('#jobsresult').slideUp("slow");
+			$('#userresult').slideUp("slow");
     	})
-    },500);
+    },200);
 }
 
 function showInteligence(data) {
 	$('#jobsresult').empty().append("<h4> Jobs </h4>");
 	$('#userresult').empty().append("<h4> Specialist </h4>");
 	$('#searchresult').slideUp("fast");
-	console.log(data);
+	
 	if(data[0].length == 0 || data[1].length == 0){
 		if(data[0].length == 0 ){
-			$('#userresult').append("<p>There are no searchresult found</p>");
-			$('#jobsresult').append("<p><a href='/jobs/details/" + data[i][y]['PK_jobId'] + "''>" + data[i][y]['title'] + "</a></p>")
+			$('#userresult').show();
+			$('#userresult').append("<p>There are no specialists found</p>");
 		}
 		if(data[1].length == 0){
-			$('#jobsresult').append("<p>There are no searchresult found</p>");
-			$('#userresult').append("<p><a href='/profile/" + data[i][y]['PK_userId'] + "''>" + data[i][y]['name'] + "</a></p>")
+			$('#jobsresult').show();
+			$('#jobsresult').append("<p>There are no jobs found</p>");
 		}
 		$('#searchresult').slideDown("slow");
 		
 	}
-	else{
-		for(i = 0; i < data.length; i++) {
-			for(y = 0; y < data[i].length; y++) {
-				if (!('name' in data[i][y])) {
-					console.log("no name");
-					if (!('title' in data[i][y])){					
-					}
-					else{
-						console.log(data[i][y]['title']);
-						$('#jobsresult').show();
-						$('#jobsresult').append("<p><a href='/jobs/details/" + data[i][y]['PK_jobId'] + "''>" + data[i][y]['title'] + "</a></p>")
-					}
+	
+	for(i = 0; i < data.length; i++) {
+		for(y = 0; y < data[i].length; y++) {
+			if (!('name' in data[i][y])) {
+				console.log("no name");
+				if (!('title' in data[i][y])){					
 				}
-				else{
-					console.log(data[i][y]['name']);
-					$('#userresult').show();
-					$('#userresult').append("<p><a href='/profile/" + data[i][y]['PK_userId'] + "''>" + data[i][y]['name'] + "</a></p>")
+				else {
+					console.log(data[i][y]['title']);
+					$('#jobsresult').show();
+					$('#jobsresult').append("<p><a href='/jobs/details/" + data[i][y]['PK_jobId'] + "''>" + data[i][y]['title'] + "</a></p>")
 				}
-
+			}
+			else{
+				console.log(data[i][y]['name']);
+				$('#userresult').show();
+				$('#userresult').append("<p><a href='/profile/" + data[i][y]['PK_userId'] + "''>" + data[i][y]['name'] + "</a></p>")
 			}
 		}
-		$('#searchresult').slideDown("slow");
 	}
+
+	$('#searchresult').slideDown("slow");
+	
 }
