593a219841985461d26e5384edd2380c4148facf
jorenvh <jorenvh@gmail.com>
2014-11-12 23:29:54 +0100

    Taal fouten verwijderd

--- app/views/content/About.blade.php
+++ app/views/content/About.blade.php
@@ -7,11 +7,11 @@
 		<p>
 			This site was created By Jonas Van der Sande & Joren Van hocht as a school project for the
 			course Web Projects. <br>
-			We had to create a project that was socialy meaningfull. Out of this came BeeHive.<br> 
+			We had to create a project that was socially meaningfull. Out of this came BeeHive.<br> 
 			BeeHive is created for people to help each other<br>
 			Everybody has something he just can't do for himself, or just can't pay for.<br>
-			That's where Beehive comes in to view. It can be a medium to meet people, make friends<br>
-			and help each other to fix things for someone else and get help in return.<br><br>
+			That's where Beehive comes in. It can be a medium to meet people, make friends<br>
+			and help each other to fix things for someone else.<br><br>
 
 			Beehive is not liable for any unforseen damage that could be done by one of our members.
 		</p>
@@ -44,7 +44,7 @@
 			</diV>
 			<p class="aboutus">
 				Hi I'm Joren and I'm a student MT at the Karel de Grote College in Antwerp.<br>
-				At a young age I started experimenting with websites and the languege that goes with them. From then on I knew
+				At a young age I started experimenting with websites and the language that goes with them. From then on I knew
 				I wanted to that all my life.
 				So to summeraise all this "I have a passion for the web!".
 			</p>
